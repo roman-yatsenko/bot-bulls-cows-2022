6bf4d76d5aaa34cab1e3e4efa59524b411f2d9dd
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-06-06 18:44:45 +0300

    Add start handler

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import telebot
+import random
 from config import bot_token
+
+bot = telebot.TeleBot(bot_token)
+
+DIGITS = [str(x) for x in range(10)]
+my_number = ''
+
+@bot.message_handler(commands=['start', 'game'])
+def start_game(message):
+    digits = DIGITS.copy()
+    my_number = ''
+    for pos in range(4):
+        if pos:
+            digit = random.choice(digits)
+        else: 
+            digit = random.choice(digits[1:])
+        my_number += digit
+        digits.remove(digit)
+    bot.reply_to(message, my_number)
+
+if __name__ == '__main__':
+    bot.polling(non_stop=True)
