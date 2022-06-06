ba7d20bb4480b64ae73500e66b69edfc232efc2c
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-06-06 19:01:35 +0300

    Add bot answer handler

--- main.py
+++ main.py
@@ -21,5 +21,14 @@ def start_game(message):
     bot.reply_to(message, 
         f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
+@bot.message_handler(content_types=['text'])
+def bot_answer(message):
+    text = message.text
+    if len(text) == 4 and text.isnumeric():
+        response = text
+    else:
+        response = 'Пришли мне 4-значное число!'
+    bot.send_message(message.from_user.id, response)
+
 if __name__ == '__main__':
     bot.polling(non_stop=True)
