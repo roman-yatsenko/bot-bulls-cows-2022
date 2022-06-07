fddc02b8e76f1df3953d1f93ede107379a0131d7
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-06-07 17:50:40 +0300

    Add number response

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ my_number = ''
 @bot.message_handler(commands=['start', 'game'])
 def start_game(message):
     digits = DIGITS.copy()
+    global my_number
     my_number = ''
     for pos in range(4):
         if pos:
@@ -18,6 +19,7 @@ def start_game(message):
             digit = random.choice(digits[1:])
         my_number += digit
         digits.remove(digit)
+    print(my_number)
     bot.reply_to(message, 
         f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
@@ -25,7 +27,14 @@ def start_game(message):
 def bot_answer(message):
     text = message.text
     if len(text) == 4 and text.isnumeric():
-        response = text
+        cows, bulls = 0, 0
+        for i in range(4):
+            if text[i] in my_number:
+                if text[i] == my_number[i]:
+                    bulls += 1
+                else:
+                    cows += 1
+        response = f'cows: {cows} | bulls: {bulls}'
     else:
         response = 'Пришли мне 4-значное число!'
     bot.send_message(message.from_user.id, response)
