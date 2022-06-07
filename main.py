a11d367da4563e830ede9be8e573cd8aa601aaf5
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-06-07 18:40:22 +0300

    Add restart keyboard

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ bot = telebot.TeleBot(bot_token)
 
 DIGITS = [str(x) for x in range(10)]
 my_number = ''
+active_game = False
 
 @bot.message_handler(commands=['start', 'game'])
 def start_game(message):
     digits = DIGITS.copy()
-    global my_number
+    global my_number, active_game
     my_number = ''
     for pos in range(4):
         if pos:
@@ -20,6 +21,7 @@ def start_game(message):
         my_number += digit
         digits.remove(digit)
     print(my_number)
+    active_game = True
     bot.reply_to(message, 'Игра "Быки и коровы"\n'
         f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
@@ -33,6 +35,7 @@ def show_help(message):
 
 @bot.message_handler(content_types=['text'])
 def bot_answer(message):
+    global active_game
     text = message.text
     if len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
         cows, bulls = 0, 0
@@ -43,12 +46,24 @@ def bot_answer(message):
                 else:
                     cows += 1
         if bulls == 4:
-            response = 'Ты угадал!'
+            active_game = False
+            response = 'Ты угадал! Сыграем еще?'
+            bot.send_message(message.from_user.id, response,
+                reply_markup=get_buttons())
+            return
         else:
             response = f'Быки: {bulls} | Коровы : {cows}'
     else:
         response = 'Пришли мне 4-значное число с разными цифрами!'
     bot.send_message(message.from_user.id, response)
+
+def get_buttons():
+    buttons = telebot.types.ReplyKeyboardMarkup(
+        one_time_keyboard=True,
+        resize_keyboard=True
+    )
+    buttons.add('Да', 'Нет')
+    return buttons
 
 if __name__ == '__main__':
     bot.polling(non_stop=True)
