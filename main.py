d50caa3500d7dd815943f49c08bac2199f5deba5
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-06-09 18:53:12 +0300

    Add level processing

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def select_level(message):
     bot.send_message(message.from_user.id, response,
         reply_markup=get_level())    
 
-def start_game(message):
+def start_game(message, level):
     digits = DIGITS.copy()
     my_number = ''
-    for pos in range(4):
+    for pos in range(level):
         if pos:
             digit = random.choice(digits)
         else: 
@@ -29,7 +29,7 @@ def start_game(message):
     with shelve.open(db_name) as storage:
         storage[str(message.from_user.id)] = my_number
     bot.reply_to(message, 'Игра "Быки и коровы"\n'
-        f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
+        f'Я загадал {level}-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
 @bot.message_handler(commands=['help'])
 def show_help(message):
@@ -45,15 +45,16 @@ def bot_answer(message):
     try:
         with shelve.open(db_name) as storage:
             my_number = storage[str(message.from_user.id)]
-        if len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
+        level = len(my_number)
+        if len(text) == level and text.isnumeric() and len(text) == len(set(text)):
             cows, bulls = 0, 0
-            for i in range(4):
+            for i in range(level):
                 if text[i] in my_number:
                     if text[i] == my_number[i]:
                         bulls += 1
                     else:
                         cows += 1
-            if bulls == 4:
+            if bulls == level:
                 print(f'{my_number} was discovered by {message.from_user.username} !')
                 with shelve.open(db_name) as storage:
                     del storage[str(message.from_user.id)]
@@ -66,10 +67,10 @@ def bot_answer(message):
             else:
                 response = f'Быки: {bulls} | Коровы : {cows}'
         else:
-            response = 'Пришли мне 4-значное число с разными цифрами!'
+            response = f'Пришли мне {level}-значное число с разными цифрами!'
     except KeyError:
         if text in ('3', '4', '5'):
-            start_game(message)
+            start_game(message, int(text))
             return
         elif text == 'Да':
             select_level(message)
